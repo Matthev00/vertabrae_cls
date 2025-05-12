68e27928c9e61ef5e4d08451b13a5da2c1042237
Matthev00 <mateuszostaszewskififa@gmail.com>
2025-05-12 19:01:51 +0200

    feat: add vertebrae extraction functionality and constants for vertebrae mapping

--- src/config.py
+++ src/config.py
@@ -19,3 +19,32 @@ SEG_MODEL_DIR = MODELS_DIR / "monai_seg"
 
 REPORTS_DIR = PROJ_ROOT / "reports"
 FIGURES_DIR = REPORTS_DIR / "figures"
+
+
+# Constants
+VERTABRAE_MAP = {
+    "L5": 18,
+    "L4": 19,
+    "L3": 20,
+    "L2": 21,
+    "L1": 22,
+    "T12": 23,
+    "T11": 24,
+    "T10": 25,
+    "T9": 26,
+    "T8": 27,
+    "T7": 28,
+    "T6": 29,
+    "T5": 30,
+    "T4": 31,
+    "T3": 32,
+    "T2": 33,
+    "T1": 34,
+    "C7": 35,
+    "C6": 36,
+    "C5": 37,
+    "C4": 38,
+    "C3": 39,
+    "C2": 40,
+    "C1": 41,
+}
--- src/data_utils/vertabra_extractor.py
+++ src/data_utils/vertabra_extractor.py
@@ -1,20 +1,34 @@
-from monai.transforms import Compose, LoadImaged, EnsureChannelFirstd, Spacingd, Orientationd, NormalizeIntensityd, EnsureTyped, ScaleIntensityd
-import torch
-from config import SEG_MODEL_DIR
-import SimpleITK as sitk
-
-from monai.bundle import ConfigParser
-import tempfile
 import os
-from monai.inferers import sliding_window_inference
+import tempfile
+from typing import Optional
 
+import numpy as np
+import SimpleITK as sitk
+import torch
+from monai.bundle import ConfigParser
+from monai.inferers import sliding_window_inference
+from monai.transforms import (
+    Compose,
+    EnsureChannelFirstd,
+    EnsureTyped,
+    LoadImaged,
+    NormalizeIntensityd,
+    Orientationd,
+    ScaleIntensityd,
+    Spacingd,
+)
+
+from config import SEG_MODEL_DIR, VERTABRAE_MAP
 
 
 class VertebraExtractor:
     """
     A class to extract specific vertebrae from CT images.
     """
-    def __init__(self, is_full_resolution: bool = False, device: torch.device = torch.device("cuda")):
+
+    def __init__(
+        self, is_full_resolution: bool = False, device: torch.device = torch.device("cuda")
+    ):
         """
         Initialize the VertebraExtractor.
 
@@ -24,20 +38,21 @@ class VertebraExtractor:
 
         """
         pixdim = (1.5, 1.5, 1.5) if is_full_resolution else (3.0, 3.0, 3.0)
-        self.transforms = Compose([
-            LoadImaged(keys=["image"]),
-            EnsureTyped(keys=["image"]),
-            EnsureChannelFirstd(keys=["image"]),
-            Orientationd(keys=["image"], axcodes="RAS"),
-            Spacingd(keys=["image"], pixdim=pixdim, mode="bilinear"),
-            NormalizeIntensityd(keys=["image"], nonzero=True),
-            ScaleIntensityd(keys=["image"], minv=-1.0, maxv=1.0)
-        ])
+        self.transforms = Compose(
+            [
+                LoadImaged(keys=["image"]),
+                EnsureTyped(keys=["image"]),
+                EnsureChannelFirstd(keys=["image"]),
+                Orientationd(keys=["image"], axcodes="RAS"),
+                Spacingd(keys=["image"], pixdim=pixdim, mode="bilinear"),
+                NormalizeIntensityd(keys=["image"], nonzero=True),
+                ScaleIntensityd(keys=["image"], minv=-1.0, maxv=1.0),
+            ]
+        )
         self.device = device
         self.is_full_resolution = is_full_resolution
         self.model = self._load_model(is_full_resolution)
 
-    
     def _load_model(self) -> torch.nn.Module:
         """
         Load the vertebra extraction model.
@@ -61,8 +76,8 @@ class VertebraExtractor:
         model.load_state_dict(weights)
         model.eval()
         return model
-    
-    def _save_tensor_as_nrrd(self, tensor: torch.Tensor, metadata: dict, filename:str) -> None:
+
+    def _save_tensor_as_nrrd(self, tensor: torch.Tensor, metadata: dict, filename: str) -> None:
         """
         Save a PyTorch tensor as an NRRD file with metadata.
 
@@ -71,17 +86,17 @@ class VertebraExtractor:
             metadata (dict): Metadata to include in the NRRD file.\
             filename (str): The filename to save the NRRD file as.
         """
-        array = tensor.numpy()  
-        
+        array = tensor.numpy()
+
         image = sitk.GetImageFromArray(array)
-        
+
         if "spacing" in metadata:
             image.SetSpacing(metadata["spacing"])
         if "direction" in metadata:
             image.SetDirection(metadata["direction"])
         if "origin" in metadata:
             image.SetOrigin(metadata["origin"])
-        
+
         sitk.WriteImage(image, filename)
 
     def _transform_data(self, tensor: torch.Tensor, metadata: dict) -> torch.Tensor:
@@ -97,14 +112,14 @@ class VertebraExtractor:
         """
         with tempfile.TemporaryDirectory() as temp_dir:
             temp_file = os.path.join(temp_dir, "output.nrrd")
-            
+
             self._save_tensor_as_nrrd(tensor, metadata, temp_file)
-            
+
             data = {"image": temp_file}
-            
+
             transformed_data = self.transforms(data)
             return transformed_data["image"]
-    
+
     def get_segmentation(self, raw_tensor: torch.Tensor, metadata: dict) -> torch.Tensor:
         """
         Get the segmentation of the input tensor.
@@ -117,20 +132,124 @@ class VertebraExtractor:
             torch.Tensor: The segmentation result.
         """
         transformed_tensor = self._transform_data(raw_tensor, metadata)
-        
+
         with torch.inference_mode():
             input_tensor = transformed_tensor.unsqueeze(0).to(self.device)
             output = sliding_window_inference(
                 inputs=input_tensor,
-                roi_size=(96, 96, 96),  
+                roi_size=(96, 96, 96),
                 sw_batch_size=1,
                 predictor=self.model,
                 padding_mode="replicate",
                 overlap=0.5,
-                mode="gaussian"
+                mode="gaussian",
             )
             output = self.model(transformed_tensor)
             output = torch.argmax(output, dim=1).squeeze(0).cpu().numpy()
-        
+
         return output
 
+    def _resize_vertabrae_tensor(
+        self,
+        input_tensor: torch.Tensor,
+        coords: np.ndarray,
+        target_size: tuple[int, int, int],
+    ) -> torch.Tensor:
+        """
+        Resize the vertebrae tensor to the target size, applying padding if necessary.
+
+        Args:
+            input_tensor (torch.Tensor): The input tensor.
+            coords (np.ndarray): Coordinates of the vertebrae.
+            target_size (tuple[int, int, int]): Target size for resizing.
+
+        Returns:
+            torch.Tensor: The resized tensor with padding if required.
+        """
+        target_coords = np.array((coords.max(axis=1) + coords.min(axis=1)) // 2)
+        z_center, y_center, x_center = target_coords
+
+        z_half, y_half, x_half = [size // 2 for size in target_size]
+
+        z_start = z_center - z_half
+        y_start = y_center - y_half
+        x_start = x_center - x_half
+
+        z_end = z_start + target_size[0]
+        y_end = y_start + target_size[1]
+        x_end = x_start + target_size[2]
+
+        pad_z_start = max(0, -z_start)
+        pad_y_start = max(0, -y_start)
+        pad_x_start = max(0, -x_start)
+
+        pad_z_end = max(0, z_end - input_tensor.shape[2])
+        pad_y_end = max(0, y_end - input_tensor.shape[3])
+        pad_x_end = max(0, x_end - input_tensor.shape[4])
+
+        z_start = max(0, z_start)
+        y_start = max(0, y_start)
+        x_start = max(0, x_start)
+
+        z_end = min(input_tensor.shape[2], z_end)
+        y_end = min(input_tensor.shape[3], y_end)
+        x_end = min(input_tensor.shape[4], x_end)
+
+        cropped_tensor = input_tensor[:, :, z_start:z_end, y_start:y_end, x_start:x_end]
+
+        if any([pad_z_start, pad_y_start, pad_x_start, pad_z_end, pad_y_end, pad_x_end]):
+            cropped_tensor = torch.nn.functional.pad(
+                cropped_tensor,
+                (pad_x_start, pad_x_end, pad_y_start, pad_y_end, pad_z_start, pad_z_end),
+                mode="constant",
+                value=0,
+            )
+
+        return cropped_tensor.detach().cpu()
+
+    def extract_vertebrae_with_neighbors(
+        self,
+        input_tensor: torch.Tensor,
+        metadata: dict,
+        target_vertabrae: str,
+        target_size: Optional[tuple[int, int, int]] = None,
+    ) -> Optional[torch.Tensor]:
+        """
+        Extract the specified vertebrae and its neighbors from the input tensor.
+
+        Args:
+            input_tensor (torch.Tensor): The input tensor.
+            metadata (dict): Metadata for the tensor.
+            target_vertabrae (str): The vertebrae to extract.
+            target_size (tuple[int, int, int], optional): The target size for resizing.
+                If None, the original size will be used.
+
+        Returns:
+            Optional[torch.Tensor]: The extracted vertebrae tensor, or None if no vertebrae are found.
+        """
+        target_label = VERTABRAE_MAP.get(target_vertabrae)
+        if target_label is None:
+            raise ValueError(f"Invalid target vertebrae: {target_vertabrae}")
+
+        labels = [target_label]
+        if target_size is None:
+            labels.extend([max(18, target_label - 1), min(41, target_label + 1)])
+
+        segmentation = self.get_segmentation(input_tensor, metadata)
+        mask = np.isin(segmentation, labels).astype(np.uint8)
+
+        coords = np.array(mask.nonzero())
+        if coords.shape[1] == 0:
+            print("No vertebrae found in the segmentation.")
+            return None
+
+        zmin, ymin, xmin = coords.min(axis=1)
+        zmax, ymax, xmax = coords.max(axis=1)
+
+        if target_size is None:
+            output = input_tensor[:, :, zmin:zmax, ymin:ymax, xmin:xmax]
+            return output.squeeze().detach().cpu()
+
+        return self._resize_vertabrae_tensor(
+            input_tensor, coords, target_size
+        )
