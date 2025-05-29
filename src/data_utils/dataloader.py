e0ff7599a6517e1629e4b917c27963f71521e1dc
Matthev00 <mateuszostaszewskififa@gmail.com>
2025-05-29 19:03:44 +0200

    refactor: moved balancing logic to separate class

--- src/data_utils/dataloader.py
+++ src/data_utils/dataloader.py
@@ -1,9 +1,6 @@
-import random
-from copy import deepcopy
 from pathlib import Path
 from typing import Optional
 
-import numpy as np
 import pandas as pd
 import torch
 from monai.transforms import (
@@ -17,131 +14,8 @@ from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
 from torchvision.transforms import Compose
 
+from src.data_utils.dataset_balancer import DatasetBalancer
 from src.data_utils.vertebrae_dataset import VertebraeDataset
-
-AUGMENT_TRANSFORMS = [
-    Rand3DElastic(
-        sigma_range=(5, 8),
-        magnitude_range=(100, 200),
-        prob=1.0,
-        spatial_size=(64, 64, 64),
-    ),
-    RandAdjustContrast(prob=1.0, gamma=(0.8, 1.2)),
-    RandAffine(
-        prob=1.0,
-        rotate_range=(0.1, 0.1, 0.1),
-        translate_range=(0.05, 0.05, 0.05),
-        scale_range=(0.9, 1.1),
-    ),
-    RandRotate(
-        prob=1.0,
-        range_x=(-0.1, 0.1),
-        range_y=(-0.1, 0.1),
-        range_z=(-0.1, 0.1),
-    ),
-    Compose(
-        [
-            Rand3DElastic(
-                sigma_range=(5, 8),
-                magnitude_range=(100, 200),
-                prob=0.5,
-                spatial_size=tuple(torch.Size([64, 64, 64])),
-            ),
-            RandAdjustContrast(prob=0.5, gamma=(0.8, 1.2)),
-            RandAffine(
-                prob=0.5,
-                rotate_range=(0.1, 0.1, 0.1),
-                translate_range=(0.05, 0.05, 0.05),
-                scale_range=(0.9, 1.1),
-            ),
-            RandRotate(
-                prob=0.5,
-                range_x=(-0.1, 0.1),
-                range_y=(-0.1, 0.1),
-                range_z=(-0.1, 0.1),
-            ),
-        ]
-    ),
-]
-
-
-def get_max_index(self, dir) -> int:
-    """
-    Gets max existing tensor index.
-
-    Returns:
-        int: max existing tensor index.
-    """
-    existing_files = list(dir.glob("*.pt"))
-    if existing_files:
-        max_index = max(int(f.stem) for f in existing_files if f.stem.isdigit())
-        return max_index
-    return 0
-
-
-def balance_dataframe_with_augmentation(
-    df: pd.DataFrame,
-    tensor_dir: Path,
-) -> pd.DataFrame:
-    """
-    Balances the dataset by augmenting samples of underrepresented classes.
-    Creates augmented tensors and saves them in a new directory.
-
-    Args:
-        df (pd.DataFrame): DataFrame containing the dataset labels.
-        tensor_dir (Path): Directory where tensor files are stored.
-
-    Returns:
-        pd.DataFrame: DataFrame with balanced dataset.
-    """
-
-    augmented_labels_path = tensor_dir.parent / "labels_augmented.csv"
-    augmented_tensor_dir = tensor_dir / "augmented"
-    augmented_tensor_dir.mkdir(parents=True, exist_ok=True)
-
-    if augmented_labels_path.exists():
-        print("[INFO] Augmented dataset found. Loading from disk.")
-        return pd.read_csv(augmented_labels_path)
-
-    class_counts = df["injury_type"].value_counts()
-    max_count = class_counts.max()
-
-    balanced_rows = [df]
-
-    class_number = 0
-
-    for cls, count in class_counts.items():
-        deficit = max_count - count
-        if deficit <= 0:
-            continue
-
-        samples_to_augment = df[df["injury_type"] == cls]
-        augment_factor = int(np.ceil(deficit / len(samples_to_augment)))
-
-        new_rows = []
-        for i in range(min(deficit, augment_factor * len(samples_to_augment))):
-            row = samples_to_augment.iloc[i % len(samples_to_augment)].copy()
-            orig_tensor_path = tensor_dir / f"{row['tensor_path']:05d}.pt"
-
-            tensor = torch.load(orig_tensor_path, weights_only=False).squeeze(0)
-
-            transforms = random.choice(AUGMENT_TRANSFORMS)
-            augmented_tensor = transforms(deepcopy(tensor))
-
-            new_tensor_id = f"{class_number + i:05d}"
-            new_tensor_path = augmented_tensor_dir / f"{new_tensor_id}.pt"
-            torch.save(augmented_tensor.unsqueeze(0), new_tensor_path)
-
-            row["tensor_path"] = f"augmented/{new_tensor_id}"
-            new_rows.append(row)
-
-        balanced_rows.append(pd.DataFrame(new_rows))
-        class_number += len(new_rows)
-
-    df_balanced = pd.concat(balanced_rows, ignore_index=True)
-    df_balanced.to_csv(augmented_labels_path, index=False)
-
-    return df_balanced
 
 
 def read_labels_file(labels_file_path: Path) -> pd.DataFrame:
@@ -205,6 +79,34 @@ def default_val_transforms() -> Compose:
     )
 
 
+def balance_dataframe(
+    df: pd.DataFrame,
+    tensor_dir: Path,
+) -> pd.DataFrame:
+    """
+    Balances the dataset by augmenting samples of underrepresented classes.
+    Creates augmented tensors and saves them in a new directory.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing the dataset labels.
+        tensor_dir (Path): Directory where tensor files are stored.
+
+    Returns:
+        pd.DataFrame: DataFrame with balanced dataset.
+    """
+    augmented_labels_path = tensor_dir.parent / "labels_augmented.csv"
+    augmented_tensor_dir = tensor_dir / "augmented"
+    augmented_tensor_dir.mkdir(parents=True, exist_ok=True)
+
+    if augmented_labels_path.exists():
+        print("[INFO] Augmented dataset found. Loading from disk.")
+        return pd.read_csv(augmented_labels_path)
+    balancer = DatasetBalancer(tensor_dir=tensor_dir)
+    balanced_df = balancer.balance_dataframe_with_augmentation(df)
+    balanced_df.to_csv(augmented_labels_path, index=False)
+    return balanced_df
+
+
 def get_dataloders(
     labels_file_path: Path,
     tensor_dir: Path,
@@ -241,12 +143,16 @@ def get_dataloders(
     train_df, val_df = train_test_split(df, train_size=train_split, random_state=42)
 
     if train_transforms is None:
-        train_transforms = default_train_transforms()
+        if balance_train:
+            train_transforms = default_val_transforms()
+        else:
+            train_transforms = default_train_transforms()
+
     if val_transforms is None:
         val_transforms = default_val_transforms()
 
     if balance_train:
-        train_df = balance_dataframe_with_augmentation(
+        train_df = balance_dataframe(
             df=train_df,
             tensor_dir=tensor_dir,
         )
@@ -289,7 +195,7 @@ if __name__ == "__main__":
         tensor_dir=tensor_directory,
         batch_size=2,
         num_workers=1,
-        balance_train=False,
+        balance_train=True,
     )
     print(f"Train Loader: {len(train_loader)} batches")
     print(f"Validation Loader: {len(val_loader)} batches")
---
+++ src/data_utils/dataset_balancer.py
@@ -0,0 +1,121 @@
+import random
+from copy import deepcopy
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import torch
+from monai.transforms import Rand3DElastic, RandAdjustContrast, RandAffine, RandRotate
+from torchvision.transforms import Compose
+
+
+class DatasetBalancer:
+    def __init__(self, tensor_dir: Path) -> None:
+        self.tensor_dir = tensor_dir
+        self.augment_transforms = [
+            Rand3DElastic(
+                sigma_range=(5, 8),
+                magnitude_range=(100, 200),
+                prob=1.0,
+                spatial_size=(64, 64, 64),
+            ),
+            RandAdjustContrast(prob=1.0, gamma=(0.8, 1.2)),
+            RandAffine(
+                prob=1.0,
+                rotate_range=(0.1, 0.1, 0.1),
+                translate_range=(0.05, 0.05, 0.05),
+                scale_range=(0.9, 1.1),
+            ),
+            RandRotate(
+                prob=1.0,
+                range_x=(-0.1, 0.1),
+                range_y=(-0.1, 0.1),
+                range_z=(-0.1, 0.1),
+            ),
+            Compose(
+                [
+                    Rand3DElastic(
+                        sigma_range=(5, 8),
+                        magnitude_range=(100, 200),
+                        prob=0.5,
+                        spatial_size=tuple(torch.Size([64, 64, 64])),
+                    ),
+                    RandAdjustContrast(prob=0.5, gamma=(0.8, 1.2)),
+                    RandAffine(
+                        prob=0.5,
+                        rotate_range=(0.1, 0.1, 0.1),
+                        translate_range=(0.05, 0.05, 0.05),
+                        scale_range=(0.9, 1.1),
+                    ),
+                    RandRotate(
+                        prob=0.5,
+                        range_x=(-0.1, 0.1),
+                        range_y=(-0.1, 0.1),
+                        range_z=(-0.1, 0.1),
+                    ),
+                ]
+            ),
+        ]
+
+    def get_max_index(self, dir) -> int:
+        """
+        Gets max existing tensor index.
+
+        Returns:
+            int: max existing tensor index.
+        """
+        existing_files = list(dir.glob("*.pt"))
+        if existing_files:
+            max_index = max(int(f.stem) for f in existing_files if f.stem.isdigit())
+            return max_index
+        return 0
+
+    def balance_dataframe_with_augmentation(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Balances the dataset by augmenting samples of underrepresented classes.
+        Creates augmented tensors and saves them in a new directory.
+
+        Args:
+            df (pd.DataFrame): DataFrame containing the dataset labels.
+
+        Returns:
+            pd.DataFrame: DataFrame with balanced dataset.
+        """
+
+        augmented_tensor_dir = self.tensor_dir / "augmented"
+        class_counts = df["injury_type"].value_counts()
+        max_count = class_counts.max()
+
+        balanced_rows = [df]
+
+        class_number = 0
+
+        for cls, count in class_counts.items():
+            deficit = max_count - count
+            if deficit <= 0:
+                continue
+
+            samples_to_augment = df[df["injury_type"] == cls]
+            augment_factor = int(np.ceil(deficit / len(samples_to_augment)))
+
+            new_rows = []
+            for i in range(min(deficit, augment_factor * len(samples_to_augment))):
+                row = samples_to_augment.iloc[i % len(samples_to_augment)].copy()
+                orig_tensor_path = self.tensor_dir / f"{row['tensor_path']:05d}.pt"
+
+                tensor = torch.load(orig_tensor_path, weights_only=False).squeeze(0)
+
+                transforms = random.choice(self.augment_transforms)
+                augmented_tensor = transforms(deepcopy(tensor))
+
+                new_tensor_id = f"{class_number + i:05d}"
+                new_tensor_path = augmented_tensor_dir / f"{new_tensor_id}.pt"
+                torch.save(augmented_tensor.unsqueeze(0), new_tensor_path)
+
+                row["tensor_path"] = f"augmented/{new_tensor_id}"
+                new_rows.append(row)
+
+            balanced_rows.append(pd.DataFrame(new_rows))
+            class_number += len(new_rows)
+
+        return pd.concat(balanced_rows, ignore_index=True)
